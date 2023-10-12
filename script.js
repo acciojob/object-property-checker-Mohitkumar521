c9bc3b5b5f123d45719d98b7375fa0447400b98a
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-12 12:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-    // Check if the key exists in the sampleObject
-    return Object.prototype.hasOwnProperty.call(sampleObject, key);
+  // Check if the key exists in the global object sampleObject
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
-// Global object
+// Global object sampleObject
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 // Examples
@@ -13,7 +11,3 @@ console.log(hasKey("red"));    // true
 console.log(hasKey("blue"));   // false
 console.log(hasKey("white"));  // true
 
-
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
